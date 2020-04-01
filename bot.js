115ae3b64eb566669f7ba389b616d17a6137ed15
WeFit Apps <wefitapps@wefit-vmelo.local>
2020-04-01 16:11:47 -0300

    q

--- bot.js
+++ bot.js
@@ -7,18 +7,20 @@ client.login('Njk0OTczMjQ1Mjg3NDMyMjIy.XoTdyw.qzqPZgiJKhb6rpkryAKM617skvM');
 client.on('ready', () => {
   console.log('The bot is up!');
   const { guilds } = client;
-  const GUILD = guilds.cache.find(guild => guild.id === serversId[Server.WEFIT]);
+  const GUILD = guilds.cache.find(guild => guild.id === '233631043851124736');
   const { channels } = GUILD;
 
   const TESTE = channels.cache.find(channel => channel.id === '694979936397557882');
   const myZohoId = '677235755';
-  const myId = '';
+  const myId = '140462634272030721';
+
+  got('http://ericktatsui.com.br:3105/').then(response => console.log('response', { response }));
 });
 
 client.on('message', message => {
   if (message.author.bot) return;
   const { guilds } = client;
-  const GUILD = guilds.cache.find(guild => guild.id === serversId[Server.WEFIT]);
+  const GUILD = guilds.cache.find(guild => guild.id === '233631043851124736');
   const { channels } = GUILD;
   const TESTE = channels.cache.find(channel => channel.id === '694979936397557882');
 
