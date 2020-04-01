5a38a2c5e0401f9561bdba9a9bf81692e13f8d9b
WeFit Apps <wefitapps@wefit-vmelo.local>
2020-04-01 15:48:55 -0300

    feat: anything

--- bot.js
+++ bot.js
@@ -1,18 +1,26 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const got = require('got');
 
 const client = new Discord.Client();
 client.login('Njk0OTczMjQ1Mjg3NDMyMjIy.XoTdyw.qzqPZgiJKhb6rpkryAKM617skvM');
 
 client.on('ready', () => {
   console.log('The bot is up!');
-  // const { guilds } = client;
-  // const wefit = guilds.cache.find(guild => guild.id === serversId[Server.WEFIT]);
-  // const { channels } = wefit;
+  const { guilds } = client;
+  const GUILD = guilds.cache.find(guild => guild.id === serversId[Server.WEFIT]);
+  const { channels } = GUILD;
 
-  // const daily = channels.cache.find(channel => channel.id === channelsId[Channel.DAILY]);
-  // startDailyDelegatesJobOnChannel(wefit);
-  // startDailyJobOnChannel(daily);
-
-  // const general = channels.cache.find(channel => channel.id === channelsId[Channel.GENERAL]);
-  // startZohoJobOnChannel(general);
+  const TESTE = channels.cache.find(channel => channel.id === '694979936397557882');
+  const myZohoId = '677235755';
+  const myId = '';
 });
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  const { guilds } = client;
+  const GUILD = guilds.cache.find(guild => guild.id === serversId[Server.WEFIT]);
+  const { channels } = GUILD;
+  const TESTE = channels.cache.find(channel => channel.id === '694979936397557882');
+
+  if (message === 'hey') TESTE.send('hello!');
+})
